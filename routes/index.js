4d2db933b2a5a5d9cac0f1fd5dee0bc1b6cc0682
Luke <winm.htun.me@gmail.com>
2016-04-17 19:49:24 +0800

    model added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var facebookBotToken = "EAAYfLQDRWHkBALmLuRuh6OOZBR6db9w9HXW1lbsy4CMYWrmWkyrOmfFWjLmvrFVTlDp7fMT0AdW3QXVW8iYPPB7ZAjlhCNJoqBsjZCY8pykoVZAPuN8F6vzOr2LfOhxso6UB3icYqaPYkyStlw12hPXWJeYZBIRzOi3jzVPi1WwZDZD";
-
+var Message = require('../models/Messages.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,7 +9,15 @@ router.get('/', function(req, res, next) {
 
 router.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === facebookBotToken) {
-    res.send(req.query['hub.challenge']);
+    console.log(req.body);
+    var newMessage = Message({
+      message: req.body.entry[0].messaging[0].message.text
+    })
+    newMessage.save(function(err){
+      if(err)
+        throw next(err);
+      console.log("message created!");
+    })
   } else {
     res.send('Error, wrong validation token');
   }
